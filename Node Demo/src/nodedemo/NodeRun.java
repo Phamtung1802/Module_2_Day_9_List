f889aea2240e2f416f584d71bdb20c99c912d27f
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-15 14:47:00 +0700

    update node demo

---
+++ Node Demo/src/nodedemo/NodeRun.java
@@ -0,0 +1,27 @@
+package nodedemo;
+
+public class NodeRun {
+    public static void main(String[] args) {
+        Node<String> head = null; //Danh sách bây giờ là rỗng
+        Node<String> tail = null;
+        Node soDauTien = new Node<>( 1.9);
+        System.out.println(soDauTien.element);
+        head=new Node<>("Chicago");
+        tail=head;
+        tail.next=new Node<>("Denver");
+        tail=tail.next;
+        tail.next=new Node<>("Apple");
+        tail=tail.next;
+        tail.next=new Node<>("Good");
+        tail=tail.next;
+        tail.next=new Node<>("Bye");
+        tail=tail.next;
+
+        Node current = head;
+        while(current != null){
+            System.out.println(current.element);
+            current = current.next;
+        }
+
+    }
+}
