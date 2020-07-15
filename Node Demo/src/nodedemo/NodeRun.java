2a7e72faafe42a07e4c5d60e8107ddbce11aaa52
Phamtung1802 <66355069+Phamtung1802@users.noreply.github.com>
2020-07-15 22:49:03 +0700

    da hieu

--- Node Demo/src/nodedemo/NodeRun.java
+++ Node Demo/src/nodedemo/NodeRun.java
@@ -14,13 +14,13 @@ public class NodeRun {
         tail.next=new Node<>("Good");
         tail=tail.next;
         tail.next=new Node<>("Bye");
+        System.out.println(head.next.next.next.data);
 
-
-        Node current = head;
-        while(current != null){
-            System.out.println(current.data);
-            current = current.next;
-        }
+//        Node current = head;
+//        while(current != null){
+//            System.out.println(current.data);
+//            current = current.next;
+//        }
 
     }
 }
