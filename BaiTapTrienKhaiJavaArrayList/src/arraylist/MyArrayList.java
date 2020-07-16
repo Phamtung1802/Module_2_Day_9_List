038b7f9835305640d7740f77cae90fd57678c7e3
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-16 12:11:13 +0700

    add chuc nang add

--- BaiTapTrienKhaiJavaArrayList/src/arraylist/MyArrayList.java
+++ BaiTapTrienKhaiJavaArrayList/src/arraylist/MyArrayList.java
@@ -1,11 +1,12 @@
 package arraylist;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MyArrayList<E> {
     private int size=0;
     static final int DEFAULT_CAPACITY=10;
-    Object element[];
+    private Object element[];
 
     public MyArrayList() {
     }
@@ -13,9 +14,42 @@ public class MyArrayList<E> {
     public MyArrayList(int initialCapacity) {
         if(initialCapacity<0)
             throw new OutOfMemoryError("Error");
-        this.size = initialCapacity;
+        this.element=new Object[initialCapacity];
     }
+
     public void add(int index, E element){
-        
+        ensureCapacity();
+        E temp=(E) this.element[index];
+        for(int j=this.element.length-1; j>index ;j--){
+            E temp2=(E)this.element[j-1];
+            this.element[j-1]=this.element[j];
+            this.element[j]=temp2;
+        }
+        this.element[index]=element;
+        this.size++;
+    }
+
+    public String printElement(int index){
+        return element[index].toString();
+    }
+
+    public String printAllElement(){
+        String arr="";
+        for(int i=0;i<this.element.length;i++){
+            arr+=" "+this.element[i].toString();
+        }
+        return arr;
+    }
+
+    public int printElementLenght(){
+        return this.element.length;
+    }
+
+    public void ensureCapacity(){
+        Object Copy[]=new Object[this.element.length+1];
+        for(int i=0;i<this.element.length;i++){
+            Copy[i]=this.element[i];
+        }
+        this.element=Copy;
     }
 }
--- BaiTapTrienKhaiJavaArrayList/src/arraylist/MyArrayListTest.java
+++ BaiTapTrienKhaiJavaArrayList/src/arraylist/MyArrayListTest.java
@@ -2,6 +2,17 @@ package arraylist;
 
 public class MyArrayListTest {
     public static void main(String[] args) {
-
+        MyArrayList<Integer> Demo=new MyArrayList<Integer>(0);
+        System.out.println("length= "+ Demo.printElementLenght());
+        Demo.add(0,3);
+        Demo.add(1,4);
+        Demo.add(2,5);
+        Demo.add(3,7);
+        System.out.println("length= "+ Demo.printElementLenght());
+        System.out.println(Demo.printAllElement());
+        Demo.add(1,6);
+        Demo.add(1,9);
+        System.out.println("length= "+ Demo.printElementLenght());
+        System.out.println(Demo.printAllElement());
     }
 }
