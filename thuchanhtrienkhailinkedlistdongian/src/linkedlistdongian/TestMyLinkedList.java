cb18b4ba4f7db2c78932971630e36f6c3db5ff72
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-16 09:45:54 +0700

    update thuchanhtrienkhailinkedlist

---
+++ thuchanhtrienkhailinkedlistdongian/src/linkedlistdongian/TestMyLinkedList.java
@@ -0,0 +1,12 @@
+package linkedlistdongian;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.add(1,14);
+        ll.printList();
+    }
+}
